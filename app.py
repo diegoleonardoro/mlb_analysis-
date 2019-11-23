65ea58489c9fcecf3d8384c39d92a2c850efb692
Diegoleoro <diegoleoro@dyn-129-236-210-180.dyn.columbia.edu>
2019-11-23 13:58:15 -0500

    Flask updates

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import psycopg2
 import sqlalchemy
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
 from flask import (
     Flask,
     render_template,
     jsonify,
     request)
-
 
 
 POSTGRES_ADDRESS = 'localhost' 
@@ -24,22 +24,37 @@ postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'
 # create the connection
 engine = create_engine(postgres_str)
 
-
 #create Flask Postgres connection:
 app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = postgres_str
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+#reflect an existing database into a new model
+Base = automap_base()
+
+#reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+batting = Base.classes.batting
+
 
 @app.route("/")
 def home():
-    
+    return render_template("index.html")
+
+
+@app.route("/columns")
+def columns():
+    stmt = db.session.query(batting).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns)[2:])
 
 
 
 
-#@app.route("/")
-#def index():
- #   return render_template("index.html")
 
 
